d6034cc526ca1d4ff11fd43c9a5817223a8afea2
Matthew Li <matthewli@D-10-18-227-148.dhcp4.washington.edu>
2017-10-27 11:10:41 -0700

    add tests, fix app

--- __tests__/app.spec.js
+++ __tests__/app.spec.js
@@ -1,6 +1,41 @@
 'use strict';
 
-import {greet, invertCase} from '../js/app'; //import the functions to test!
+import { greet, invertCase } from '../js/app'; //import the functions to test!
 
 /* tests go here */
 
+
+describe('Basic Math', function () {
+
+    it('should add numbers correctly', function () {
+
+        let sum = 1 + 1;
+
+        // make assertion
+        expect(sum).toEqual(2);
+    });
+
+});
+
+describe('invert() function', function() {
+
+    it('should invert from all caps', function() {
+        let returnValue = invertCase("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+
+        expect(returnValue).toEqual("abcdefghijklmnoqprstuvwxyz");
+    });
+
+    it('should invert from all lowercase', function() {
+        expect(invertCase("abcdefghijklmnoqprstuvwxyz")).toEqual("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    });
+    
+    it('should work with mixed', function() {
+        let returnValue = invertCase("aBcDefgHijkLmnopqRsTuvwXyZ");
+        expect(returnValue).toEqual("AbCdEFGhIJKlMNOQPrStUVWxYz");
+    });
+
+    it('should ignore spaces', function() {
+        expect(invertCase("hello world")).toEqual("HELLO WORLD");
+    });
+
+});
--- js/app.js
+++ js/app.js
@@ -11,8 +11,8 @@ function greet(name){
  * It may have a few small bugs...
  */
 function invertCase(text) {
-  const LOWER_CASE = "abedefghijklmnoqprstuvwxyz";
-  const UPPER_CASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const LOWER_CASE = "abcdefghijklmnoqprstuvwxyz ";
+  const UPPER_CASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
 
   let tempString = '';
   for(let i=0; i<text.length; i++){
@@ -22,11 +22,11 @@ function invertCase(text) {
       tempString += upper;
     }
     else { //is uppercase
-      let lower = LOWER_CASE [ LOWER_CASE.indexOf(char) ];
+      let lower = LOWER_CASE [ UPPER_CASE.indexOf(char) ];
       tempString += lower;
     }
   }
-  return text;
+  return tempString;
 }
 
 
